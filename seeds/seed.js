e674b07e2df7aed8ee26a37b43195accbc262918
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-12-06 22:31:28 -0600

    User.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -19,6 +19,8 @@ const seedDatabase = async () => {
             user_id: users[Math.floor(Math.random() * users.length)].id,
         });
     }
+    await commentData();
+    
     process.exit(0);
 }
 
