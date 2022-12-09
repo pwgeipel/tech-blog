39c99bdf8d067a30501cdcef1ea326afd42bbf51
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-12-08 23:28:01 -0600

    readme

---
+++ controllers/api/blogRoutes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const { User, Blog, Comment } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+        const newBlog
+    }
+})
