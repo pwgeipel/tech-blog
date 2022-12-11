04cee082a0f3cf93434e0e98f0eb857556a4cca8
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-12-11 14:25:59 -0600

    user routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,12 +1,16 @@
 const router = require('express').Router();
-const { User, Post, Comment } = require('../../models');
+const { User, Blog, Comment } = require('../../models');
 
 router.post('/', async (req, res) => {
     try {
-        const userData = await User.create(req.body);
+        const userData = await User.create({
+            username: req.body.username,
+            password: req.body.password,
+        });
 
         req.session.save(() => {
             req.session.user_id = userData.id;
+            req.session.username = userData.username;
             req.session.logged_in = true;
 
             res.status(200).json(userData);
@@ -18,7 +22,7 @@ router.post('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
     try {
-        const userData = await User.findOne({ where: { email: req.body.email }});
+        const userData = await User.findOne({ where: { username: req.body.email }});
 
         if (!userData) {
             res.status(400).json({ message: 'Incorrect email or password.  Please try again.' });
@@ -34,6 +38,7 @@ router.post('/login', async (req, res) => {
 
         req.session.save(() => {
             req.session.user_id = userData.id;
+            req.session.username = userData.username;
             req.session.logged_in = true;
 
             res.json({ user: userData, message: "You are now logged in!"});
