fc9d7877f7e39d5c784b7c0b79bd6dd780ec5268
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-12-11 17:44:07 -0600

    blogRoutes

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
+const withAuth = require('../../utils/authentication');
 const { User, Blog, Comment } = require('../../models');
+const bodyParser = require('body-parser');
 
-// router.post('/', async (req, res) => {
-//     try {
-//         const newBlog
-//     }
-// })
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const blogData = await Blog.create({ ...body, userId: req.session.userId});
+        res.json(newBlog);
 
 module.exports = router;
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Blog, Comment } = require('../../models');
+const { User } = require('../../models');
 
 router.post('/', async (req, res) => {
     try {
