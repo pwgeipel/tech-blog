93f611634b1d9925554443424f4ea8d71010f289
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-12-11 18:35:01 -0600

    blogRoutes delete

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,11 +1,33 @@
 const router = require('express').Router();
 const withAuth = require('../../utils/authentication');
-const { User, Blog, Comment } = require('../../models');
-const bodyParser = require('body-parser');
+const { Blog } = require('../../models');
 
-router.get('/', withAuth, async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
+    const body = req.body;
     try {
-        const blogData = await Blog.create({ ...body, userId: req.session.userId});
+        const newBlog = await Blog.create({ ...body, userId: req.session.userId});
         res.json(newBlog);
+    } catch (err) {
+        console.log('Failure to create new blog post.', err);
+        res.status(500).json(err)
+    }
+});
+
+router.delete('/', withAuth, async (req, res) => {
+    try {
+        const blogData = Blog.destroy({
+            where: {
+                id: req.params.id,
+            },
+        }),
+        if (!blogData) {
+            res.status(404).json({ message: 'Blog post with this id not found.' });
+            return;
+        }
+        res.status(200).json(blogData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
