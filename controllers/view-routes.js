94a35902c9d051429ba3e26423437b5153b43304
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-12-30 09:21:27 -0600

    view-routes

--- controllers/view-routes.js
+++ controllers/view-routes.js
@@ -2,14 +2,25 @@ const router = require('express').Router()
 const { User, Blog, Comment } = require('../models')
 
 router.get('/', async (req, res) => {
-    let blogs = await Blog.findAll()
-    blogs = blogs.map(blog => blog.get({ plain: true }))
-    res.render('home', { blogs })
+    try {    
+        let blogs = await Blog.findAll()
+        blogs = blogs.map(blog => blog.get({ plain: true }))
+        res.render('home', { blogs })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
 
-    router.get('/blog/:id', (req, res) => {
+router.get('/blog/:id', async (req, res) => {
+    try {
+        let blog = await Blog.findByPk(req.params.id)
+        blog = blog.get({ plain: true })
 
-    })
-
+        res.render('blog', { blog })
+        
+    } catch(err) {
+        res.status(500).json(err)
+    }    
 })
 
 module.exports = router;
